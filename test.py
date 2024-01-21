5655b954c80b36bea4d7498f7f78ff80078909ba
Steve C <BuzzKanga@users.noreply.github.com>
2024-01-22 07:42:35 +1100

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import requests
 
-url = "http://localhost:8080/2015-03-31/functions/function/invocations"
+# url = "http://localhost:8080/2015-03-31/functions/function/invocations"
+url = "https://nekc22pysf.execute-api.ap-southeast-2.amazonaws.com/test/predict"
 
 data = {
-    "url": "https://raw.githubusercontent.com/BuzzKanga/MLZoomCamp-CNN-CatBreedClassification/master/Abyssinian_1.jpg"
+    "url": "https://raw.githubusercontent.com/BuzzKanga/MLZoomCamp-CNN-CatBreedClassification/master/Abyssinian_104.jpg"
 }
 
 result = requests.post(url, json=data).json()
